74d5e18c00feec4081bcdbf0fe3067e97d48ce5e
Ludvigpbf <ludvig.flyckt@gmail.com>
2023-05-11 15:09:04 +0200

    uppdaterade recipeSchema och userSchema

--- server/src/models/RecipeSchema.js
+++ server/src/models/RecipeSchema.js
@@ -1,27 +1,20 @@
 import mongoose from "mongoose";
 
 const RecipeSchema = new mongoose.Schema({
+  author: { type: String, required: true },
   title: { type: String, required: true },
   picture: { type: String, required: true },
   description: { type: String, required: true },
-  ingredients: { type: Array, required: true },
-  time: { type: Number, required: false },
-  steps: {
-    1: { type: String, required: false },
-    2: { type: String, required: false },
-    3: { type: String, required: false },
-    4: { type: String, required: false },
-    5: { type: String, required: false },
-    6: { type: String, required: false },
-    7: { type: String, required: false },
-    8: { type: String, required: false },
-    9: { type: String, required: false },
-    10: { type: String, required: false },
+  ingredients: {
+    name: { type: String, required: true },
+    amount: { type: Number, required: true },
+    unit: { type: String, required: true },
   },
+  duration: { type: Number, required: false },
+  steps: { type: Array, required: true },
   reviewScore: { type: Number, required: false },
   likes: { type: Number, required: false },
   date: { type: Date, required: true },
-  /* userName: { type: String, required: true }, */
 });
 
 export const UserModel = mongoose.model("Recipe", RecipeSchema);
--- server/src/models/Users.js
+++ server/src/models/Users.js
@@ -1,13 +1,11 @@
 import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
-    username: { type: String,  required: true },
-    password: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    profilePicture: { type: String, required: false },
-    bio: { type: String, required: false },
-})
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  profilePicture: { type: String, required: false },
+  bio: { type: String, required: false },
+});
 
-
-
-export const UserModel = mongoose.model('users', UserSchema)
+export const UserModel = mongoose.model("users", UserSchema);
