8e67858b54a8766d03c5c7c5b521ce50b9c0d914
Your Name <joselyn.moreta@chasacademy.se>
2023-05-15 08:09:25 +0200

    createpost

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import CreatePost from './pages/CreatePost.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <CreatePost />
   </React.StrictMode>,
 )
+
+
