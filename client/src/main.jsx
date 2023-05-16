49e0c2880032a04c441d89c93b6fe86af48dd020
Amilia Waloh <amilija@live.se>
2023-05-16 14:58:29 +0200

    lagt till routerprovider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import UserProfile from './pages/userProfile.jsx'
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "userprofile",
+    element: 
+    <>
+    <UserProfile />
+    </>
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
+  <RouterProvider router={router} />
 )
--- client/src/pages/UserProfile.jsx
+++ client/src/pages/UserProfile.jsx
@@ -1,1 +1,12 @@
-import { useState } from "react";
+
+
+function UserProfile() {
+
+    return (
+        <>
+            <h1>Hej</h1>
+        </>
+    ) 
+}
+
+export default UserProfile
