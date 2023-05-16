f4eed93770bffbcd9ded61929c158baf38e33f0c
Amilia Waloh <amilija@live.se>
2023-05-16 15:13:03 +0200

    preliminär router i main

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,17 @@
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+//import App from './App.jsx'
 import UserProfile from './pages/userProfile.jsx'
+import './index.css'
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 const router = createBrowserRouter([
+  {
+    path: "/",
+    element: 
+    <>
+      {/* LÄGG APP HÄR? */}
+    </>
+  },
   {
     path: "userprofile",
     element: 
@@ -15,6 +22,5 @@ const router = createBrowserRouter([
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />,
   <RouterProvider router={router} />
 )
