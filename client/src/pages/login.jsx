54a41f2be191d7a3dd9165464917b6300ae3559c
BakerAlblam <gallardo0@outlook.com>
2023-05-16 16:32:26 +0200

    reg o login
    klara

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -23,26 +23,32 @@ const Login = () => {
   };
 
   return (
-    <div className="text-center text-3xl my-10 ">
-      <form onSubmit={onSubmit}>
-        <h1 className="text-red-500"> Login </h1>
-        <label htmlFor="email">Email</label>
-        <input
-          type="text"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          id="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button type="submit"> Login </button>
-      </form>
-    </div>
+    <section className="bg-gray-50">
+  <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+     
+      <div className="w-full bg-white rounded-lg shadow-xl dark:border md:mt-0 sm:max-w-md xl:p-0  ">
+          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+              <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl  text-center">
+                  Login
+              </h1>
+              <form className="space-y-4 md:space-y-6" onSubmit={onSubmit}>
+                  <div>
+                      <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"></label>
+                      <input type="email" name="email" id="email" value={email}onChange={(e) => setEmail(e.target.value)} className="bg-gray-50 border-2 border-gray-900 text-gray-900 sm:text-sm rounded-3xl block w-full p-2.5" placeholder="Email"  />
+                  </div>
+    
+                  <div>
+                      <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"></label>
+                      <input type="password" id="Password" placeholder="Password"value={password} onChange={(e) => setPassword(e.target.value)} className="bg-gray-50 border-2 border-gray-900 text-gray-900 sm:text-sm rounded-3xl block w-full p-2.5"   />
+                  </div>
+               
+                  <button type="submit" className="w-full focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-3xl text-sm px-5 py-2.5 text-center bg-black text-white">Create an account</button>
+                 
+              </form>
+          </div>
+      </div>
+  </div>
+</section>
   );
 };
 
