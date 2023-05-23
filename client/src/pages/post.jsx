646de1f6c26d144bcb45123ceff32dc3fa80de37
Your Name <joselyn.moreta@chasacademy.se>
2023-05-24 01:53:51 +0200

    ändrat bild

--- client/src/pages/post.jsx
+++ client/src/pages/post.jsx
@@ -16,7 +16,7 @@ function post() {
     
 <a href="#" className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-x hover:bg-gray-100 dark:border-white-900 dark:bg-white-800 dark:hover:bg-white-700">
   
-    <img className="w-full max-w-2xl	max-h-2x1 object-contain" src="https://editorialtelevisa.brightspotcdn.com/dims4/default/bf3664a/2147483647/strip/true/crop/889x500+56+0/resize/1440x810!/quality/90/?url=https%3A%2F%2Fk2-prod-editorial-televisa.s3.amazonaws.com%2Fbrightspot%2Fwp-content%2Fuploads%2F2018%2F04%2F10-Cuentas-de-Instagram-que-debes-seguir-si-eres-amante-de-los-postres.jpeg" alt=""/>
+    <img className="w-full max-w-2xl	max-h-2x1 object-contain" src="https://img.koket.se/standard-mega/pannkaka-ny.jpg" alt=""/>
    
     <div className="flex flex-col justify-between p-2 mb-10   leading-normal">
 
@@ -24,27 +24,27 @@ function post() {
         <span className="text-gray-600 text-xs block mb-5 ">Sweden, Malmö</span>
         <div className="flex items-center justify-between mx-1 mt-1 mb-3 ">
       <div className="flex mr-20 gap-4">
-        <button className='px-1'><svg fill="#262626" height="24" viewBox="0 0 48 48" width="24"><path d="M34.6 6.1c5.7 0 10.4 5.2 10.4 11.5 0 6.8-5.9 11-11.5 16S25 41.3 24 41.9c-1.1-.7-4.7-4-9.5-8.3-5.7-5-11.5-9.2-11.5-16C3 11.3 7.7 6.1 13.4 6.1c4.2 0 6.5 2 8.1 4.3 1.9 2.6 2.2 3.9 2.5 3.9.3 0 .6-1.3 2.5-3.9 1.6-2.3 3.9-4.3 8.1-4.3m0-3c-4.5 0-7.9 1.8-10.6 5.6-2.7-3.7-6.1-5.5-10.6-5.5C6 3.1 0 9.6 0 17.6c0 7.3 5.4 12 10.6 16.5.6.5 1.3 1.1 1.9 1.7l2.3 2c4.4 3.9 6.6 5.9 7.6 6.5.5.3 1.1.5 1.6.5.6 0 1.1-.2 1.6-.5 1-.6 2.8-2.2 7.8-6.8l2-1.8c.7-.6 1.3-1.2 2-1.7C42.7 29.6 48 25 48 17.6c0-8-6-14.5-13.4-14.5z"></path></svg></button>
+        <button className='px-2 h-4 '><svg fill="#262626" height="24" viewBox="0 0 48 48" width="24"><path d="M34.6 6.1c5.7 0 10.4 5.2 10.4 11.5 0 6.8-5.9 11-11.5 16S25 41.3 24 41.9c-1.1-.7-4.7-4-9.5-8.3-5.7-5-11.5-9.2-11.5-16C3 11.3 7.7 6.1 13.4 6.1c4.2 0 6.5 2 8.1 4.3 1.9 2.6 2.2 3.9 2.5 3.9.3 0 .6-1.3 2.5-3.9 1.6-2.3 3.9-4.3 8.1-4.3m0-3c-4.5 0-7.9 1.8-10.6 5.6-2.7-3.7-6.1-5.5-10.6-5.5C6 3.1 0 9.6 0 17.6c0 7.3 5.4 12 10.6 16.5.6.5 1.3 1.1 1.9 1.7l2.3 2c4.4 3.9 6.6 5.9 7.6 6.5.5.3 1.1.5 1.6.5.6 0 1.1-.2 1.6-.5 1-.6 2.8-2.2 7.8-6.8l2-1.8c.7-.6 1.3-1.2 2-1.7C42.7 29.6 48 25 48 17.6c0-8-6-14.5-13.4-14.5z"></path></svg></button>
        
       </div>
-      <div className="flex ">
-  <button><svg fill="#262626" height="34" viewBox="0 0 28 28" width="50"><path d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z"></path></svg></button> </div>
+      <div className="flex  ">
+  <button className="w-8 ml-60  h-6 text-yellow-400"><svg aria-hidden="true"  fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Rating star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg></button> </div>
+  <p className="ml-0 text-sm font-bold text-gray-900 dark:text-dark">4.95</p>
+
 </div>  
 <hr className=' h-0.5 dark:bg-gray-700' /> 
-
 
    <div className="flex ">
    <div className="m-3 grid grid-cols-10 ">
   <div className="flex rounded-xl  bg-white border border-gray-200 rounded-lg shadow p-1 col-span-10">
-  <div className="flex mt-4  pt-14 ">
-    <svg aria-hidden="true" className="w-5  h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Rating star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
-    <p className="ml-3 text-sm font-bold text-gray-900 dark:text-dark">4.95</p>
-</div>
-    <div className="m-1  flex h-14 w-14 items-center justify-center overflow-hidden rounded-full flex-shrink-0">
-      <img  src="https://w7.pngwing.com/pngs/134/447/png-transparent-united-states-pepe-the-frog-anti-defamation-league-hillary-clinton-presidential-campaign-2016-meme-united-states.png" />
-    </div>
+  <div className="flex flex-col  px-3 pt-2 ">
+      <img  className="m-1 flex h-12 w-12 items-center justify-center overflow-hidden rounded-full flex-shrink-0"
+         src="https://w7.pngwing.com/pngs/134/447/png-transparent-united-states-pepe-the-frog-anti-defamation-league-hillary-clinton-presidential-campaign-2016-meme-united-states.png" />
+           <svg aria-hidden="true" className="w-5  h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Rating star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+              <p className="ml-3 text-sm font-bold text-gray-900 dark:text-dark">4.95</p></div>
+   
   
-    <div className="ml-4 w-full">
+    <div className="ml-5 p-3 w-full">
       <div className="flex w-full items-center justify-between">
         <h2 className="text-sm font-medium">Maria</h2>
         <p  className="text-sm ">19 hours ago</p>
@@ -60,16 +60,13 @@ function post() {
 
   <div className="col-span-10 col-start-3 mt-4 space-y-4">
     <div className="flex   bg-white border border-gray-200 rounded-lg shadow p-4">
-    <div className="flex mt-4  pt-14 ">
-    <svg aria-hidden="true" className="w-5  h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Rating star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
-    <p className="ml-3 text-sm font-bold text-gray-900 dark:text-dark">4.95</p>
-</div>
-      <div className="mx-2 flex h-12 w-12 items-center justify-center overflow-hidden rounded-full flex-shrink-0">
-        <img src="https://e7.pngegg.com/pngimages/475/527/png-clipart-amphibian-pepe-the-frog-meme-anime-amphibian-face-animals-thumbnail.png" />
-      
-      </div>
+      <div className="flex flex-col  px-3 pt-2 ">
+      <img  className="m-1 flex h-12 w-12 items-center justify-center overflow-hidden rounded-full flex-shrink-0"
+         src="https://e7.pngegg.com/pngimages/475/527/png-clipart-amphibian-pepe-the-frog-meme-anime-amphibian-face-animals-thumbnail.png" />
+           <svg aria-hidden="true" className="w-5  h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Rating star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+              <p className="ml-3 text-sm font-bold text-gray-900 dark:text-dark">4.95</p></div>
 
-      <div className="ml-2 w-full">
+      <div className="ml-5 p-2 w-full h-25">
         <div className="flex w-full items-center justify-between">
           <h2 className="text-sm font-medium">Jasmine</h2>
           <p className="text-sm ">12 hours ago</p>
@@ -90,8 +87,8 @@ function post() {
 
 <hr className=' h-0.5 dark:bg-gray-700' /> 
 <div className='flex  flex-nowrap'>
-<div className='w-60 m-5 '>
-     <input type="text" placeholder='Lägg till kommentar...' name='' id='add-comment'  className=" h-20  " />
+<div className='w-60 m-5  '>
+     <input type="text" placeholder='Lägg till kommentar...' name='' id='add-comment'  className=" ml-5 p-2 w-full h-25  " />
  </div>
 
 <div className=' flex flex-row-reverse m-2 px-9 '>
@@ -105,7 +102,7 @@ function post() {
                 </g>
             </g>
         </svg>
-        <select className="text-sm text-white font-bold rounded bg-gray-500 border border-grey h-8 w-60 pl-2 pr-10 bg-white hover:border-gray-400 focus:outline-none appearance-none">
+        <select className="text-sm text-white font-bold rounded bg-gray-500 border border-grey h-8 w-60 pl-2 pr-8 bg-white hover:border-gray-400 focus:outline-none appearance-none">
             <option>⭐0</option>
             <option>⭐1</option>
             <option>⭐1.5</option>
