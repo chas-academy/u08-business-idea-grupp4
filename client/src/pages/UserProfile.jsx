6144389c87c95b8023c94fc8a1ae32e7609455a0
Amilia Waloh <amilija@live.se>
2023-05-23 20:55:50 +0200

    hårdkodat innehåll i userprofile

--- client/src/pages/UserProfile.jsx
+++ client/src/pages/UserProfile.jsx
@@ -5,7 +5,7 @@ function UserProfile() {
     return (
         <>
         <div className="flex justify-center">
-            <div className="flex justify-between lg:w-7/12 w-11/12 pt-20 pb-10">
+            <div className="flex justify-between lg:w-7/12 w-11/12 sm:pt-20 pt-10 pb-10 sm:px-12">
                 <div>
                     <img className="sm:h-48 sm:w-48 w-32 h-32 rounded-full" src="https://ichef.bbci.co.uk/news/976/cpsprodpb/16620/production/_91408619_55df76d5-2245-41c1-8031-07a4da3f313f.jpg" alt="Profile Picture"/>
                 </div>
@@ -16,7 +16,7 @@ function UserProfile() {
                             <p>recipes</p>
                         </div>
                         <div className="flex flex-col text-center">
-                            <h4 className="sm:text-2xl">500</h4>
+                            <h4 className="sm:text-2xl">500k</h4>
                             <p>followers</p>
                         </div>
                         <div className="flex flex-col text-center">
@@ -32,8 +32,8 @@ function UserProfile() {
         </div>
         <div className="flex justify-center">
             <div className="flex flex-col lg:w-7/12 w-11/12 space-y-3">
-                <h5 className="text-xs font-bold">UserName</h5>
-                <p>Bio/Description</p>
+                <h5 className="text-xs font-bold">mrfroggie</h5>
+                <p>🐸🍳 Cooking aficionado and ice cream enthusiast 🍦🔥 Sharing my culinary adventures and mouthwatering recipes with a splash of creativity! 🌱🌍 #FoodieFroggie #IceCreamDelights</p>
             </div>
         </div>
         
@@ -88,17 +88,56 @@ function UserProfile() {
 
         <div className="flex justify-center pt-5">
         <div className="grid grid-cols-3 sm:gap-3 gap-1 lg:w-7/12 w-11/12">
-            <div className="bg-gray-200 sm:h-48 h-28">bild</div>
-            <div className="bg-gray-200 sm:h-48 h-28">bild</div>
-            <div className="bg-gray-200 sm:h-48 h-28">bild</div>
-            <div className="bg-gray-200 sm:h-48 h-28">bild</div>
-            <div className="bg-gray-200 sm:h-48 h-28">bild</div>
-            <div className="bg-gray-200 sm:h-48 h-28">bild</div>
-            <div className="bg-gray-200 sm:h-48 h-28">bild</div>
-            <div className="bg-gray-200 sm:h-48 h-28">bild</div>
-            <div className="bg-gray-200 sm:h-48 h-28">bild</div>
-            <div className="bg-gray-200 sm:h-48 h-28">bild</div>
-            <div className="bg-gray-200 sm:h-48 h-28">bild</div>
+            <div className="bg-gray-200 sm:h-48 h-28">
+                <Link to="">
+                    <img className="h-full w-full" src="https://images.unsplash.com/photo-1546069901-ba9599a7e63c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1160&q=80" alt="Profile Picture"/>
+                </Link>
+            </div>
+            <div className="bg-gray-200 sm:h-48 h-28">
+            <Link to="">
+                <img className="h-full w-full" src="https://images.unsplash.com/photo-1555939594-58d7cb561ad1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=987&q=80" alt="Profile Picture"/>
+            </Link>
+            </div>
+            <div className="bg-gray-200 sm:h-48 h-28">
+            <Link to="">
+                <img className="h-full w-full" src="https://images.unsplash.com/photo-1497034825429-c343d7c6a68f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=987&q=80" alt="Profile Picture"/>
+            </Link>
+            </div>
+            <div className="bg-gray-200 sm:h-48 h-28">
+            <Link to="">
+                <img className="h-full w-full" src="https://images.unsplash.com/photo-1529042410759-befb1204b468?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=772&q=80" alt="Profile Picture"/>
+            </Link>
+            </div>
+            <div className="bg-gray-200 sm:h-48 h-28">
+            <Link to="">
+                <img className="h-full w-full" src="https://images.unsplash.com/photo-1476224203421-9ac39bcb3327?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80" alt="Profile Picture"/>
+            </Link>
+            </div>
+            <div className="bg-gray-200 sm:h-48 h-28">
+            <Link to="">
+                <img className="h-full w-full" src="https://images.unsplash.com/photo-1482049016688-2d3e1b311543?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=820&q=80" alt="Profile Picture"/>
+            </Link>
+            </div>
+            <div className="bg-gray-200 sm:h-48 h-28">
+            <Link to="">
+                <img className="h-full w-full" src="https://images.unsplash.com/photo-1565958011703-44f9829ba187?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1065&q=80" alt="Profile Picture"/>
+            </Link>
+            </div>
+            <div className="bg-gray-200 sm:h-48 h-28">
+            <Link to="">
+                <img className="h-full w-full" src="https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1081&q=80" alt="Profile Picture"/>
+            </Link>
+            </div>
+            <div className="bg-gray-200 sm:h-48 h-28">
+            <Link to="">
+                <img className="h-full w-full" src="https://plus.unsplash.com/premium_photo-1663853560438-6cc3a70a3c97?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=987&q=80" alt="Profile Picture"/>
+            </Link>
+            </div>
+            <div className="bg-gray-200 sm:h-48 h-28">
+            <Link to="">
+                <img className="h-full w-full" src="https://ichef.bbci.co.uk/news/976/cpsprodpb/16620/production/_91408619_55df76d5-2245-41c1-8031-07a4da3f313f.jpg" alt="Profile Picture"/>
+            </Link>
+            </div>
         </div>
         </div>
         </>
