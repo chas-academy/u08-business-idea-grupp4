9ccc34190775eecf6e688b70f2dd561e6c72e5ec
Hafiz Samaae <hafez@hotmail.com>
2023-05-24 13:47:04 +0200

    uppdaterqt fil

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+const App = () => {
   return (
     <>
-      <p className="text-red-600">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ client/src/Hafiz.jsx
@@ -0,0 +1,89 @@
+import React, { useState } from 'react';
+import Search from './Hafiz';
+
+const Search = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [filter, setFilter] = useState('all');
+
+  const foodItems = [
+    { name: 'Burger', category: 'food' },
+    { name: 'Pizza', category: 'food' },
+    { name: 'Pasta', category: 'food' },
+    { name: 'Cheesecake', category: 'cake' },
+    { name: 'Chocolate cake', category: 'cake' },
+    { name: 'Coffee', category: 'drink' },
+    { name: 'Tea', category: 'drink' },
+    { name: 'Cola', category: 'drink' },
+  ];
+
+  const handleSearch = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const handleFilter = (category) => {
+    setFilter(category);
+  };
+
+  const filteredItems = foodItems.filter((item) => {
+    if (filter === 'all') {
+      return item.name.toLowerCase().includes(searchTerm.toLowerCase());
+    } else {
+      return (
+        item.category === filter &&
+        item.name.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+    }
+  });
+
+  return (
+    <div className="container mx-auto p-4">
+      <input
+        type="text"
+        className="bg-gray-50 border-2 border-gray-900 text-gray-900 sm:text-sm rounded-3xl block w-full p-2.5 "
+        placeholder="Search"
+        onChange={handleSearch}
+      />
+      <div className="flex gap-2 mb-4">
+        <button
+          className={`py-2 px-4 rounded-md ${
+            filter === 'all' ? 'bg-blue-500 text-white' : 'bg-gray-200'
+          }`}
+          onClick={() => handleFilter('all')}
+        >
+          All
+        </button>
+        <button
+          className={`py-2 px-4 rounded-md ${
+            filter === 'food' ? 'bg-blue-500 text-white' : 'bg-gray-200'
+          }`}
+          onClick={() => handleFilter('food')}
+        >
+          Food
+        </button>
+        <button
+          className={`py-2 px-4 rounded-md ${
+            filter === 'cake' ? 'bg-blue-500 text-white' : 'bg-gray-200'
+          }`}
+          onClick={() => handleFilter('cake')}
+        >
+          Cakes
+        </button>
+        <button
+          className={`py-2 px-4 rounded-md ${
+            filter === 'drink' ? 'bg-blue-500 text-white' : 'bg-gray-200'
+          }`}
+          onClick={() => handleFilter('drink')}
+        >
+          Drinks
+        </button>
+      </div>
+      <ul className="list-disc pl-6">
+        {filteredItems.map((item, index) => (
+          <li key={index}>{item.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Search;
