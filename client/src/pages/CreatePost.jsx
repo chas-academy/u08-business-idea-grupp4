3e569e8b3e18c61759643ceedf768813b023c037
“Joselyn <joselyn.moreta@chasacademy.se>
2023-05-31 11:21:52 +0200

    createpost

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 function CreatePost() {
 
   return (
-    <div className ="bg-gray-50 dark:bg-gray-900">
+    
+   
+   
+   <div className ="bg-gray-50 flex justify-center mr-50  p-50 dark:bg-gray-900">
   
-          <div > 
-<h2 className='font-serif  text-3xl m-5'>Create Post</h2>
-
-          </div>
+         
           <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
         <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
   
@@ -24,22 +24,23 @@ function CreatePost() {
   
   
     
-          <input type="text" placeholder='Instructions' className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-white-700 dark:border-gray-600 dark:placeholder-gray-400  dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'/>
-          <input type="text" placeholder='Cooking Time' className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-white-700 dark:border-gray-600 dark:placeholder-gray-400  dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'/>
-          <input type="text" placeholder='Shoping list URL' className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-white-700 dark:border-gray-600 dark:placeholder-gray-400  dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'/>
-  
-       </div>
-        <button 
-        className="w-60 text-white bg-blue-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 p-2.5 mb-5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+      <input type="text" placeholder='Upload Picture                                                                  +' 
+      className='bg-gray-50 border border-black text-gray-900 sm:text-sm rounded-full  block w-full p-2.5 dark:bg-white-700 '/>
+      <input type="text" placeholder='Description' className='bg-gray-50 border border-black text-gray-900 sm:text-sm rounded-full  block w-full p-2.5 dark:bg-white-700 '/>
+          <input type="text" placeholder='Ingredients' className='bg-gray-50 border border-black text-gray-900 sm:text-sm rounded-full  block w-full p-2.5 dark:bg-white-700   '/>
+          <input type="text" placeholder='Instructions' className='bg-gray-50 border border-black text-gray-900 sm:text-sm rounded-full  block w-full p-2.5 dark:bg-white-700 '/>
+          <input type="text" placeholder='Cooking Time' className='bg-gray-50 border border-black text-gray-900 sm:text-sm rounded-full  block w-full p-2.5 dark:bg-white-700 '/>
+          <input type="text" placeholder='Shopping list URL' className='bg-gray-50 border border-black text-gray-900 sm:text-sm rounded-full  block w-full p-2.5 dark:bg-white-700 '/>
+    <button 
+        className=" bg-blue-600 border border-black text-white sm:text-sm rounded-full focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-white-700 dark:border-gray-600   dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
        
         >
       POST
-  </button>
-  
-      </div>
-  
-  
-      </div>
+      </button> 
+     </div> 
+     
+   </div>
+  </div>
   )
 }
 
