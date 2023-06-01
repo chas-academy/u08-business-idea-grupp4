99c56f5b72f9cc19e98a0bf11bbc3390c0a49afe
oscar Björk <occe.bjork@outlook.com>
2023-06-01 10:47:21 +0200

    catigory

--- client/src/pages/CreateCategory.jsx
+++ client/src/pages/CreateCategory.jsx
@@ -14,59 +14,59 @@ export default function CreateCategory() {
                     </div>
                 </div>
             </div>
-            <div className="flex justify-center items-center px-4 sm:px-6 lg:px-8">
-                <div className="grid grid-cols-3 sm:grid-cols-3 md:grid-cols-3 gap-6 items-center justify-items-center">
+            <div className="flex justify-center items-center px-4 mr-32 sm:px-6 lg:px-8">
+                <div className="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-3 gap-24 items-center justify-items-center">
                     
                     <div className="w-full sm:w-auto space-between">
-                        <div className="border border-gray-300 rounded p-12 sm:p-24 mb-10 mx-1">
+                        <div className="border border-gray-300 rounded p-12 sm:p-18 lg:p-24 mb-10 mx-10">
                         <input id="default-checkbox" type="checkbox" value="" className="w-5 h-5 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
                         </div>
                     </div>
                     <div className="w-full sm:w-auto space-between">
-                        <div className="border border-gray-300 rounded p-12 sm:p-24 mb-10 mx-7">
+                        <div className="border border-gray-300 rounded p-12 sm:p-18 lg:p-24 mb-10 mx-10">
                         <input id="default-checkbox" type="checkbox" value="" className="w-5 h-5 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
                         </div>
                     </div>
                     <div className="w-full sm:w-auto space-between">
-                        <div className="border border-gray-300 rounded p-12 sm:p-24 mb-10 mx-12">
+                        <div className="border border-gray-300 rounded p-12 sm:p-18 lg:p-24 mb-10 mx-10">
                         <input id="default-checkbox" type="checkbox" value="" className="w-5 h-5 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
                         </div>
                     </div>
                     <div className="w-full sm:w-auto space-between">
-                        <div className="border border-gray-300 rounded p-12 sm:p-24 mb-10 mx-1">
+                        <div className="border border-gray-300 rounded p-12 sm:p-18 lg:p-24 mb-10 mx-10">
                         <input id="default-checkbox" type="checkbox" value="" className="w-5 h-5 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
                         </div>
                     </div>
                     <div className="w-full sm:w-auto space-between">
-                        <div className="border border-gray-300 rounded p-12 sm:p-24 mb-10 mx-7">
+                        <div className="border border-gray-300 rounded p-12 sm:p-18 lg:p-24 mb-10 mx-10">
                         <input id="default-checkbox" type="checkbox" value="" className="w-5 h-5 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
                         </div>
                     </div>
                     <div className="w-full sm:w-auto space-between">
-                        <div className="border border-gray-300 rounded p-12 sm:p-24 mb-10 mx-12">
+                        <div className="border border-gray-300 rounded p-12 sm:p-18 lg:p-24 mb-10 mx-10">
                         <input id="default-checkbox" type="checkbox" value="" className="w-5 h-5 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
                         </div>
                     </div>
                     <div className="w-full sm:w-auto space-between">
-                        <div className="border border-gray-300 rounded p-12 sm:p-24 mb-10 mx-1">
+                        <div className="border border-gray-300 rounded p-12 sm:p-18 lg:p-24 mb-10 mx-10">
                         <input id="default-checkbox" type="checkbox" value="" className="w-5 h-5 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
                         </div>
                     </div>
                     <div className="w-full sm:w-auto space-between">
-                        <div className="border border-gray-300 rounded p-12 sm:p-24 mb-10 mx-7">
+                        <div className="border border-gray-300 rounded p-12 sm:p-18 lg:p-24 mb-10 mx-10">
                         <input id="default-checkbox" type="checkbox" value="" className="w-5 h-5 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
                         </div>
                     </div>   
                     <div className="w-full sm:w-auto space-between">
-                        <div className="border border-gray-300 rounded p-12 sm:p-24 mb-10 mx-12">
+                        <div className="border border-gray-300 rounded p-12 sm:p-18 lg:p-24 mb-10 mx-10">
                         <input id="default-checkbox" type="checkbox" value="" className="w-5 h-5 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
                         </div>
                     </div>   
                 </div>
             </div>
 
-            <div className="flex justify-center items-center m-12 py-2 w-full">
-                <button className="w-full sm:w-auto px-10">
+            <div className="flex justify-center items-center m-12 py-2">
+                <button className="w-full sm:w-auto px-24 items-center">
                     <div className="bg-gray-300 hover:bg-gray-800 text-gray-900 font-semibold py-2 px-4 sm:px-10 border border-gray-400 rounded shadow bg-transparent hover:text-white background-black">
                         ADD
                     </div>
--- server/src/routes/category.js
+++ server/src/routes/category.js
@@ -5,7 +5,8 @@ const router = express.Router()
 
 router.get("/", async (req, res) => {
     return res.status(200).json({
-        message: "Hello World",
+        message: "Hello World,",
+       
     });
 })
 
