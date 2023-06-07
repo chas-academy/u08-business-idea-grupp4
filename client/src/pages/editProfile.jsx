c21fe6cc39935128bd21e81b58fa665d5ecb3adc
Amilia Waloh <amilija@live.se>
2023-06-07 15:08:14 +0200

    fixade imports

--- client/src/pages/editProfile.jsx
+++ client/src/pages/editProfile.jsx
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import axios from 'axios';
 import { useCookies } from "react-cookie";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
 
 const EditProfile = () => {
   const [username, setUsername] = useState('');
