c132207afa9baf345ae41ed919125db831bb2998
Ludvigpbf <ludvig.flyckt@gmail.com>
2023-06-11 18:56:58 +0200

    fixar connection

--- server/src/index.js
+++ server/src/index.js
@@ -25,9 +25,7 @@ app.use("/post", postRouter);
 app.use("/category", categoryRouter);
 
 mongoose
-  .connect(
-    "mongodb+srv://bakerhaider:hmshood@cluster0.ts1n8aq.mongodb.net/Cluster0?retryWrites=true&w=majority"
-  )
+  .connect("mongodb+srv://bakerhaider:hmshood@cluster0.ts1n8aq.mongodb.net/")
   .then(() => {
     console.log("Connected to MongoDB");
   })
